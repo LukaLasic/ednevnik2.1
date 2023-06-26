028ce7a2a9aba2416729987b278cf1af452eca17
Luka Lasić <luka.lasic@fsre.sum.ba>
2023-06-26 19:47:52 +0200

    Admin

--- src/main/java/net/fgrprojekt/User.java
+++ src/main/java/net/fgrprojekt/User.java
@@ -20,17 +20,19 @@ public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "first_name", nullable = false, length = 20)
-	private String firstName;
-	@Column(name = "last_name", nullable = false, length = 20)
-	private String lastName;
+
+	 Long id;
+	@Column(name = "firstName", nullable = false, length = 20)
+	 String firstName;
+	@Column(name = "lastName", nullable = false, length = 20)
+	 String lastName;
 	@Column(name = "email",nullable = false, unique = true, length = 45)
-	private String email;
+	 String email;
 	@Column(name = "password",nullable = false, length = 64)
-	private String password;
+
+	 String password;
 	@Column(name = "role",nullable = false)
-	private String role;
+	 String role;
 
 	// Constructors, getters, and setters
 
